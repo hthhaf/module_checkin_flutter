feaf192f5932e6977e1101be1c85d77b1c7f1062
HTHHAF <59130386+hthhaf@users.noreply.github.com>
2023-10-11 16:45:47 +0700

    Update http_endpoints.dart

--- lib/state/services/http/http_endpoints.dart
+++ lib/state/services/http/http_endpoints.dart
@@ -1,13 +1,13 @@
 class Endpoints {
-  static const String baseUrl = 'https://apiv2.vietravelairlines.online/api/';
+  
   static const int receiveTimeout = 30;
   static const int connectionTimeout = 30;
 
-  static const String authEndpoint = 'User/LoginSTD';
-  static const String retrievePnrEndpoint = 'ResReservation/RetrievePNR';
+  static const String authEndpoint = 'LoginSTD';
+  static const String retrievePnrEndpoint = 'RetrievePNR';
   static const String retrieveSeatAvailabilityList =
-      'ResFlight/RetrieveSeatAvailabilityList';
-  static const String seatAssignmentEndpoint = 'ResSeating/SeatAssignment';
-  static const String checkInEndpoint = 'ResCheckIn/CheckInAsync';
-  static const String savePnrEndpoint = 'ResReservation/SavePNR';
+      'RetrieveSeatAvailabilityList';
+  static const String seatAssignmentEndpoint = 'SeatAssignment';
+  static const String checkInEndpoint = 'CheckInAsync';
+  static const String savePnrEndpoint = 'SavePNR';
 }
