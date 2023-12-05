4ebdb525cb193c56a28775e6cbd237ac0e87cca3
HTHHAF <59130386+hthhaf@users.noreply.github.com>
2023-12-05 10:51:02 +0700

    Update app.constant.dart

--- lib/ulitites/app.constant.dart
+++ lib/ulitites/app.constant.dart
@@ -1,5 +1,4 @@
 class AppConstant {
-  static const String baseUrl = 'https://erptuat.vietravelairlines.vn';
-  //static const String baseUrl = 'http://10.0.2.2:7122/';
+  
   static const String keyToken = 'key_token';
 }
